751495eeb4465a615dd6c5a3dce0dd3bcc3a5b9c
Tas-fique <infinitebatarang@outlook.com>
2025-09-07 23:57:27 -0400

    Create myMean.R

---
+++ module2/myMean.R
@@ -0,0 +1,10 @@
+assignment2 <- c(16,18,14,22,27,17,19,17,17,22,20,22)
+
+myMean <- function(assignment2) {
+  sum(assignment2) / length(assignment2)
+}
+
+myMean(assignment2)  # expect 19.25
+
+
+Module 2: add corrected myMean and test
